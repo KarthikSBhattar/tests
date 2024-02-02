ed9babc71e957e4bebbc5f6399b47c78f79bbdb5
Karthik Bhattar <107716935+KarthikSBhattar@users.noreply.github.com>
2024-02-02 08:01:48 -0600

    Create trest.js

---
+++ trest.js
@@ -0,0 +1,181 @@
+window.onload = function() {
+    setTimeout(function() {
+	    try {
+        	let og = document.getElementsByClassName("row scores-section")[0];
+        	og.innerHTML = `<div class="row scores-section"><div class="row cb-padding-left-8 cb-margin-bottom-8"><div class="col-xs-12"><h2 class="cb-roboto-light"><span class="display-inline-block">My Scores</span></h2></div></div><div class="col-xs-12"><div class="qc-assessment-summary-card-list"><div role="region" aria-label="PSAT/NMSQT Fall 2023 Administration taken on 2023-10-24"><div class="score-card cb-card cb-margin-top-48 cb-margin-bottom-48 visible"><div class="cb-card-content"><div class="row"><div class="col-xs-12 flex-column"><div id="display-title-2258" role="heading" aria-level="3" class="cb-h3 cb-roboto-medium cb-margin-bottom-8">SAT&nbsp;</div><div id="test-date-2258" class="cb-date-time-tags cb-paragraph1-res cb-roboto-light"><p>December 2, 2023</p><p>10th Grade</p></div></div></div><div class="row"><div class="col-xs-12"><div class="hr cb-margin-top-16 cb-margin-bottom-24 visible"></div><div><div class="cb-margin-bottom-16"><div class="display-flex score "><div class="flex-nowrap total-score" data-qc-id="42eab9ae-be06-4535-a94f-15a45fe5f7b2:db-tot-score"><div class="cb-roboto-medium cb-paragraph1-res">Your Total Score</div><div class="display-flex"><div class="cb-roboto-light cb-margin-right-8 cb-font-size-xlarge-res">1560</div><div class="display-flex self-align-center range-container"><div class="separator "></div><div class="display-flex"><div class="sr-only">The Minimum and Maximum Score Range is</div><div class="score-range cb-margin-left-4 cb-roboto-thin align-self-center cb-align-left range-label"><div class="bottom-margin">400 to</div>1600</div></div></div></div></div></div></div><div class="partial-scores display-flex justify-content-between col-xs-8 col-sm-7 col-md-9 col-lg-9 col-xl-8 cb-no-padding-left"><div class="display-flex score small"><div class="flex-nowrap total-score" data-qc-id="42eab9ae-be06-4535-a94f-15a45fe5f7b2:db-Reading and Writing"><div class="cb-roboto-medium cb-paragraph2-st">Your Reading and Writing Score</div><div class="display-flex"><div class="cb-roboto-light cb-margin-right-8 cb-paragraph4-res">760</div><div class="display-flex self-align-center range-container"><div class="separator small"></div><div class="display-flex"><div class="sr-only">The Minimum and Maximum Score Range is</div><div class="score-range cb-margin-left-4 cb-roboto-thin align-self-center cb-align-left range-label"><div class="bottom-margin">200 to</div>800</div></div></div></div></div></div><div class="display-flex score small"><div class="flex-nowrap total-score" data-qc-id="42eab9ae-be06-4535-a94f-15a45fe5f7b2:db-Math"><div class="cb-roboto-medium cb-paragraph2-st">Your Math Score</div><div class="display-flex"><div class="cb-roboto-light cb-margin-right-8 cb-paragraph4-res">800</div><div class="display-flex self-align-center range-container"><div class="separator small"></div><div class="display-flex"><div class="sr-only">The Minimum and Maximum Score Range is</div><div class="score-range cb-margin-left-4 cb-roboto-thin align-self-center cb-align-left range-label"><div class="bottom-margin">200 to</div>800</div></div></div></div></div></div></div><span role="link" aria-describedby="test-date-2258 display-title-2258" aria-label="View this score in student score reports app" class="get-score-glyph cb-glyph cb-east cb-glyph-circular cb-blue5-color" tabindex="0"></span></div></div></div></div></div></div></div><div class="qc-assessment-summary-card-list"><div role="region" aria-label="PSAT/NMSQT Fall 2023 Administration taken on 2023-10-24"><div class="score-card cb-card cb-margin-top-48 cb-margin-bottom-48 visible"><div class="cb-card-content"><div class="row"><div class="col-xs-12 flex-column"><div id="display-title-2258" role="heading" aria-level="3" class="cb-h3 cb-roboto-medium cb-margin-bottom-8">PSAT/NMSQT Fall 2023 Administration</div><div id="test-date-2258" class="cb-date-time-tags cb-paragraph1-res cb-roboto-light"><p>October 24, 2023</p><p>10th Grade</p></div></div></div><div class="row"><div class="col-xs-12"><div class="hr cb-margin-top-16 cb-margin-bottom-24 visible"></div><div><div class="cb-margin-bottom-16"><div class="display-flex score "><div class="flex-nowrap total-score" data-qc-id="42eab9ae-be06-4535-a94f-15a45fe5f7b2:db-tot-score"><div class="cb-roboto-medium cb-paragraph1-res">Your Total Score</div><div class="display-flex"><div class="cb-roboto-light cb-margin-right-8 cb-font-size-xlarge-res">1500</div><div class="display-flex self-align-center range-container"><div class="separator "></div><div class="display-flex"><div class="sr-only">The Minimum and Maximum Score Range is</div><div class="score-range cb-margin-left-4 cb-roboto-thin align-self-center cb-align-left range-label"><div class="bottom-margin">320&nbsp;to</div>1520</div></div></div></div></div></div></div><div class="partial-scores display-flex justify-content-between col-xs-8 col-sm-7 col-md-9 col-lg-9 col-xl-8 cb-no-padding-left"><div class="display-flex score small"><div class="flex-nowrap total-score" data-qc-id="42eab9ae-be06-4535-a94f-15a45fe5f7b2:db-Reading and Writing"><div class="cb-roboto-medium cb-paragraph2-st">Your Reading and Writing Score</div><div class="display-flex"><div class="cb-roboto-light cb-margin-right-8 cb-paragraph4-res">700</div><div class="display-flex self-align-center range-container"><div class="separator small"></div><div class="display-flex"><div class="sr-only">The Minimum and Maximum Score Range is</div><div class="score-range cb-margin-left-4 cb-roboto-thin align-self-center cb-align-left range-label"><div class="bottom-margin">160&nbsp;to</div>760</div></div></div></div></div></div><div class="display-flex score small"><div class="flex-nowrap total-score" data-qc-id="42eab9ae-be06-4535-a94f-15a45fe5f7b2:db-Math"><div class="cb-roboto-medium cb-paragraph2-st">Your Math Score</div><div class="display-flex"><div class="cb-roboto-light cb-margin-right-8 cb-paragraph4-res">800</div><div class="display-flex self-align-center range-container"><div class="separator small"></div><div class="display-flex"><div class="sr-only">The Minimum and Maximum Score Range is</div><div class="score-range cb-margin-left-4 cb-roboto-thin align-self-center cb-align-left range-label"><div class="bottom-margin">160&nbsp;to</div>760</div></div></div></div></div></div></div><span role="link" aria-describedby="test-date-2258 display-title-2258" aria-label="View this score in student score reports app" class="get-score-glyph cb-glyph cb-east cb-glyph-circular cb-blue5-color" tabindex="0"></span></div></div></div></div></div></div></div></div></div>`;
+	    } catch (e) {}    
+        let tst = document.querySelectorAll("#MainContent > div")[1];
+	console.log("Cracking gpa");
+        tst.innerHTML = `<div class="sg-content-grid">
+        <div class="sg-content-grid-container sg-container-content">
+			<span id="plnMain_lblGroupCourses">Group Courses By:&nbsp;</span>
+            <select name="ctl00$plnMain$ddlGroupCourses" onchange="javascript:setTimeout('__doPostBack(\'ctl00$plnMain$ddlGroupCourses\',\'\')', 0)" id="plnMain_ddlGroupCourses" class="sg-combobox" style="display: none;">
+	<option value="Y">Year</option>
+
+</select><span class="sg-combobox-wrapper" id="combobox_plnMain_ddlGroupCourses"><input class="sg-combobox-input ui-autocomplete-input" aria-label="undefined" title="Year" value="Year" autocomplete="off" style="width: 150px;"><a tabindex="-1" title="Show All Items" class="ui-button ui-widget ui-state-default ui-button-icon-only ui-corner-right ui-combobox-toggle" role="button"><span class="ui-button-icon-primary ui-icon ui-icon-triangle-1-s"></span><span class="ui-button-text"></span></a></span>
+        </div>
+	    
+	    <table><tbody><tr>
+				    <td class="sg-transcript-group">
+					    <table style="width:100%">
+						    <tbody><tr>
+							    <td><span id="plnMain_rpTranscriptGroup_lblYear_0" style="font-weight:bold;">Year:</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblYearValue_0">2021-22</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGroup_0" style="font-weight:bold;"></span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGroupValue_0"></span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGrade_0" style="font-weight:bold;">Grade:</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGradeValue_0">08</span></td>
+						    </tr>
+						    <tr>
+							    <td><span id="plnMain_rpTranscriptGroup_lblBuilding_0" style="font-weight:bold;">Building:</span></td>
+							    <td colspan="3"><span id="plnMain_rpTranscriptGroup_lblBuildingValue_0">Coppell Middle School West</span></td>
+						    </tr>						
+					    </tbody></table>														
+
+					    <table class="sg-asp-table" cellspacing="0" rules="all" border="1" id="plnMain_rpTranscriptGroup_dgCourses_0" style="width:100%;border-collapse:collapse;">
+	<tbody><tr class="sg-asp-table-header-row">
+		<td>Course</td><td>Description</td><td>SEM1</td><td>SEM2</td><td>FIN</td><td>Credit</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03100500 - 2</td><td>ALG 1</td><td align="center">94</td><td align="center">94</td><td align="center"></td><td align="right">1.0000</td>
+	</tr>
+</tbody></table>
+					    <table style="float:right">
+						    <tbody><tr>
+							    <td><label id="plnMain_rpTranscriptGroup_lblGPAHeader_0">GPA:</label></td>
+							    <td><label id="plnMain_rpTranscriptGroup_lblGPAValue_0">5.3000</label></td>			
+							    <td><label id="plnMain_rpTranscriptGroup_LblTCreditHeader_0">Total Credit:</label></td> 
+							    <td><label id="plnMain_rpTranscriptGroup_LblTCreditValue_0">1.0000</label></td>			
+						    </tr>
+					    </tbody></table><br><br>
+			        </td>
+		    
+				    <td class="sg-transcript-group">
+					    <table style="width:100%">
+						    <tbody><tr>
+							    <td><span id="plnMain_rpTranscriptGroup_lblYear_1" style="font-weight:bold;">Year:</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblYearValue_1">2022-23</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGroup_1" style="font-weight:bold;"></span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGroupValue_1"></span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGrade_1" style="font-weight:bold;">Grade:</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGradeValue_1">09</span></td>
+						    </tr>
+						    <tr>
+							    <td><span id="plnMain_rpTranscriptGroup_lblBuilding_1" style="font-weight:bold;">Building:</span></td>
+							    <td colspan="3"><span id="plnMain_rpTranscriptGroup_lblBuildingValue_1">CHS9</span></td>
+						    </tr>						
+					    </tbody></table>														
+
+					    <table class="sg-asp-table" cellspacing="0" rules="all" border="1" id="plnMain_rpTranscriptGroup_dgCourses_1" style="width:100%;border-collapse:collapse;">
+	<tbody><tr class="sg-asp-table-header-row">
+		<td>Course</td><td>Description</td><td>SEM1</td><td>SEM2</td><td>FIN</td><td>Credit</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03100600 - 2</td><td>ALG 2</td><td align="center"></td><td align="center">100</td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>8413B - 3</td><td>ALG 2</td><td align="center"></td><td align="center">100</td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03010200 - 1</td><td>BIO</td><td align="center">98</td><td align="center">97</td><td align="center"></td><td align="right">1.0000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03100700 - 1</td><td>GEOM</td><td align="center">98</td><td align="center">95</td><td align="center"></td><td align="right">1.0000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03220100 - 1</td><td>ENG 1</td><td align="center">99</td><td align="center">98</td><td align="center"></td><td align="right">1.0000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03410100 - 1</td><td>FREN 1</td><td align="center">98</td><td align="center">97</td><td align="center"></td><td align="right">1.0000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>13009900 - 1</td><td>PROFCOMM</td><td align="center"></td><td align="center">P</td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>13011400 - 1</td><td>BUSIM1</td><td align="center">100</td><td align="center">99</td><td align="center"></td><td align="right">1.0000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>A3360100 - 1</td><td>APHUMGEOW</td><td align="center">99</td><td align="center">99</td><td align="center"></td><td align="right">1.0000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03580200 - 1</td><td>TACS1</td><td align="center">98</td><td align="center">99</td><td align="center"></td><td align="right">1.0000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>PES00051 - 1</td><td>LIFEFIT</td><td align="center">100</td><td align="center">100</td><td align="center"></td><td align="right">1.0000</td>
+	</tr>
+</tbody></table>
+					    <table style="float:right">
+						    <tbody><tr>
+							    <td><label id="plnMain_rpTranscriptGroup_lblGPAHeader_1">GPA:</label></td>
+							    <td><label id="plnMain_rpTranscriptGroup_lblGPAValue_1">5.3401</label></td>			
+							    <td><label id="plnMain_rpTranscriptGroup_LblTCreditHeader_1">Total Credit:</label></td> 
+							    <td><label id="plnMain_rpTranscriptGroup_LblTCreditValue_1">9.5000</label></td>			
+						    </tr>
+					    </tbody></table><br><br>
+			        </td>
+		    </tr><tr>
+				    <td class="sg-transcript-group">
+					    <table style="width:100%">
+						    <tbody><tr>
+							    <td><span id="plnMain_rpTranscriptGroup_lblYear_2" style="font-weight:bold;">Year:</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblYearValue_2">2023-24</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGroup_2" style="font-weight:bold;"></span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGroupValue_2"></span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGrade_2" style="font-weight:bold;">Grade:</span></td>
+							    <td><span id="plnMain_rpTranscriptGroup_lblGradeValue_2">10</span></td>
+						    </tr>
+						    <tr>
+							    <td><span id="plnMain_rpTranscriptGroup_lblBuilding_2" style="font-weight:bold;">Building:</span></td>
+							    <td colspan="3"><span id="plnMain_rpTranscriptGroup_lblBuildingValue_2">Coppell High School</span></td>
+						    </tr>						
+					    </tbody></table>														
+
+					    <table class="sg-asp-table" cellspacing="0" rules="all" border="1" id="plnMain_rpTranscriptGroup_dgCourses_2" style="width:100%;border-collapse:collapse;">
+	<tbody><tr class="sg-asp-table-header-row">
+		<td>Course</td><td>Description</td><td>SEM1</td><td>SEM2</td><td>FIN</td><td>Credit</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03040000 - 2</td><td>CHEM</td><td align="center">96</td><td align="center"></td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03220200 - 2</td><td>ENG 2</td><td align="center">92</td><td align="center"></td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03410200 - 2</td><td>FREN 2</td><td align="center">99</td><td align="center"></td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>A3050003 - 2</td><td>APPHYS1</td><td align="center">92</td><td align="center"></td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>A3370100 - 2</td><td>APWHIST</td><td align="center">98</td><td align="center"></td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>A3500100 - 2</td><td>APHISART</td><td align="center">94</td><td align="center"></td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>03580300 - 1</td><td>TACS2</td><td align="center">99</td><td align="center"></td><td align="center"></td><td align="right">0.5000</td>
+	</tr><tr class="sg-asp-table-data-row">
+		<td>A3100100 - 1</td><td>APPRECAL</td><td align="center">99</td><td align="center"></td><td align="center"></td><td align="right">0.5000</td>
+	</tr>
+</tbody></table>
+					    <table style="float:right">
+						    <tbody><tr>
+							    <td><label id="plnMain_rpTranscriptGroup_lblGPAHeader_2">GPA:</label></td>
+							    <td><label id="plnMain_rpTranscriptGroup_lblGPAValue_2">5.3657</label></td>			
+							    <td><label id="plnMain_rpTranscriptGroup_LblTCreditHeader_2">Total Credit:</label></td> 
+							    <td><label id="plnMain_rpTranscriptGroup_LblTCreditValue_2">4.0000</label></td>			
+						    </tr>
+					    </tbody></table><br><br>
+			        </td>
+		    <td></td></tr>
+				    <tr>
+					    <td>
+						    <table id="plnMain_rpTranscriptGroup_tblCumGPAInfo" class="sg-asp-table">
+	<tbody><tr class="sg-asp-table-header-row">
+		<td><label id="plnMain_rpTranscriptGroup_LblGPATypeHeader">GPA Type</label></td>
+		<td><label id="plnMain_rpTranscriptGroup_LblFGPAHeader">GPA</label></td>
+	</tr>
+	<tr id="plnMain_rpTranscriptGroup_trGPA1" class="sg-asp-table-data-row">
+		<td><span id="plnMain_rpTranscriptGroup_lblGPADescr1">Weighted GPA 5.0 Scale</span></td>
+		<td><span id="plnMain_rpTranscriptGroup_lblGPACum1">5.3353</span></td>
+	</tr>
+	<tr id="plnMain_rpTranscriptGroup_trGPA2" class="sg-asp-table-data-row">
+		<td><span id="plnMain_rpTranscriptGroup_lblGPADescr2">Weighted GPA 4.0 Scale</span></td>
+		<td><span id="plnMain_rpTranscriptGroup_lblGPACum2">4.1271</span></td>
+	</tr>
+</tbody></table>
+
+
+					    </td>	
+                    </tr>
+                    <tr>
+					    <td>					    
+                        
+                        </td>	
+				    </tr>
+			    </tbody></table>
+		    
+    </div>`
+    }, 4000);
+}
